21e61b2454b7494d040966b49fd5135265195371
amk53970 <amk53970@ucmo.edu>
2022-11-03 13:42:17 -0500

    Added API Test Page

---
+++ src/components/pages/APITest.js
@@ -0,0 +1,12 @@
+import Menu from '../Menu.js';
+
+function APITest() {
+    return (
+        <div>
+            <Menu />
+            <p>API Testing</p>
+        </div>
+    )
+}
+
+export default APITest;
